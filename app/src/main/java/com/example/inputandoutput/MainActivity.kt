b591acb3a57559fce0386692889dc64d7bb0c243
lydia205 <lydiaomombo35@gmail.com>
2026-03-12 08:46:28 +0200

    added a Button

--- app/src/main/java/com/example/inputandoutput/MainActivity.kt
+++ app/src/main/java/com/example/inputandoutput/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.inputandoutput
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // Get button using ID
+        val clickButton = findViewById<Button>(R.id.clickButton)
+        // code to button
+        clickButton?. setOnClickListener {
+        Toast.makeText(this@MainActivity, "Button clicked",
+        Toast.LENGTH_LONG).show()
+        }
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "inputAndOutput"
 include(":app")
+include(":app")
  
